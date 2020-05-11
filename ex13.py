c7c5d52a44ebc99c368744fedca2813c0a0e0e2f
Jonathan Baumert <jonathanbaumert@yahoo.com>
2020-05-10 23:57:04 -0700

    first commit and add ex13 and ex14

---
+++ ex13.py
@@ -0,0 +1,7 @@
+from sys import argv
+
+script, first, second, third = argv
+print("The script is called:",script)
+print("First var called:",first)
+print("Second var called:",second)
+print("Third var called:",third)
